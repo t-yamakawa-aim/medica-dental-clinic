6cb1c347344127cb4b3599941907724b18127107
t-yamakawa-aim <peacefultomorrow0528@gmail.com>
2026-08-14 20:38:27 +0900

    Update FacilitySection.tsx

--- src/components/FacilitySection.tsx
+++ src/components/FacilitySection.tsx
@@ -24,13 +24,13 @@ export const FacilitySection = () => {
 
       <div class="facility-gallery">
         <div class="facility-gallery__item">
-          <img src="/static/images/facility-01.jpg" alt="施設紹介" />
+          <img src="/static/images/facility-01.png" alt="施設紹介" />
         </div>
         <div class="facility-gallery__item">
-          <img src="/static/images/facility-02.jpg" alt="施設紹介" />
+          <img src="/static/images/facility-02.png" alt="施設紹介" />
         </div>
         <div class="facility-gallery__item">
-          <img src="/static/images/facility-03.jpg" alt="施設紹介" />
+          <img src="/static/images/facility-03.png" alt="施設紹介" />
         </div>
       </div>
     </section>
